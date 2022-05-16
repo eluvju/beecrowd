34b21658ee998d5955f47450bfd7a348245f2b57
Emerson Vieira <eluvju@gmail.com>
2022-05-16 04:22:26 -0300

    Uso de %(resto) de forma otimizada

---
+++ Beginner/1018.py
@@ -0,0 +1,18 @@
+N = int(input())
+
+N100,RESTON100 = int((N/100)),(N%100)
+N50,RESTON50 = int((RESTON100/50)),(RESTON100%50)
+N20,RESTON20 = int((RESTON50/20)),(RESTON50%20)
+N10,RESTON10 = int((RESTON20/10)),(RESTON20%10)
+N5,RESTON5 = int((RESTON10/5)),(RESTON10%5)
+N2,RESTON2 = int((RESTON5/2)),(RESTON5%2)
+N1,RESTON1 = int((RESTON2/1)),(RESTON2%1)
+
+print(f'{N:.0f}')
+print(f'{N100} nota(s) de R$ 100,00')
+print(f'{N50} nota(s) de R$ 50,00')
+print(f'{N20} nota(s) de R$ 20,00')
+print(f'{N10} nota(s) de R$ 10,00')
+print(f'{N5} nota(s) de R$ 5,00')
+print(f'{N2} nota(s) de R$ 2,00')
+print(f'{N1} nota(s) de R$ 1,00')
