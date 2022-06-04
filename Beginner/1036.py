e10be0ff9359652d7e992b13d1e7c1afb3fdbc1d
Emerson Vieira <eluvju@gmail.com>
2022-06-03 21:36:42 -0300

    03/06

---
+++ Beginner/1036.py
@@ -0,0 +1,19 @@
+# Read 3 floating-point numbers. After, print the roots of bhaskara’s formula. If it's impossible to calculate the roots because a division by zero or a square root of a negative number, presents the message “Impossivel calcular”.
+
+# Input
+# Read 3 floating-point numbers (double) A, B and C.
+
+# Output
+# Print the result with 5 digits after the decimal point or the message if it is impossible to calculate
+
+A, B, C = map(float, input().split(" "))
+
+DELTA = ((B**2)-(4*A*C))
+
+if A != 0 and DELTA > 0:
+    R1 = (((-B)+(DELTA**0.5))/(2*A))
+    R2 = (((-B)-(DELTA**0.5))/(2*A))
+    print(f"R1 = {R1:.5f}")
+    print(f"R2 = {R2:.5f}")
+else:
+    print('Impossivel calcular')
